7468610723bfce5a9b72d3b753bf11e5e0a8ec65
Artur Sarkisyan <go.ar2r@gmail.com>
2013-12-09 22:00:09 +0200

    Add class GrabberBackground that returns backgrounds for movie, add to module can_haz_poster self method grab_bg wich will call class method and return the backgrounds for movie, add integration test on class GrabberBackground and fixtures for it

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -16,3 +16,24 @@ describe CanHazPoster::Grabber do
     posters.should == "http://www.movieposterdb.com/posters/11_01/2006/381061/l_381061_3b296a44.jpg"
   end
 end
+
+describe CanHazPoster::GrabberBackground do
+  before do
+    stub_http_request(:get, "https://www.themoviedb.org/search?query=The%20Matrix").
+      to_return(body: File.new('spec/fixtures/bg_search_result.html'), status: 200)
+
+    stub_http_request(:get, "https://www.themoviedb.org/movie/603-the-matrix").
+      to_return(body: File.new('spec/fixtures/bg_movie_page.html'), status: 200)
+
+    stub_http_request(:get, "https://www.themoviedb.org/movie/603-the-matrix/backdrops").
+      to_return(body: File.new('spec/fixtures/bg_show_all.html'), status: 200)
+
+    stub_http_request(:get, "https://www.themoviedb.org/movie/603-the-matrix/images?kind=backdrop&language=&translate=false").
+      to_return(body: File.new('spec/fixtures/bg_collection.html'), status: 200)
+  end
+
+  it "finds movie background by title and year" do
+    background = subject.grab_bg("The Matrix", 1999)
+    background.should include("https://d3gtl9l2a4fn1j.cloudfront.net/t/p/original/7u3pxc0K1wx32IleAkLv78MKgrw.jpg")
+  end
+end
