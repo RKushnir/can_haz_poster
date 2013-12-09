dd36858da94939cf51cc449973665551b53c6103
Artur Sarkisyan <go.ar2r@gmail.com>
2013-12-09 12:32:03 +0200

    Move class GrabberBackground to CanHazPOster module

--- lib/can_haz_background.rb
+++ lib/can_haz_background.rb
@@ -2,7 +2,7 @@
 module CanHazBackground
   MovieNotFoundError = Class.new(StandardError)
 
-  autoload :GrabberBackground, './can_haz_background/grabber_background'
+  autoload :GrabberBackground, 'can_haz_background/grabber_background'
 
   def self.grab_bg(title, year)
     GrabberBackground.new.grab_bg(title, year)
